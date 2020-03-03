6b68cc32998402258d5be7f5740cd11ea67f56e1
kingsleylam <kingsleyl.0120@gmail.com>
2020-03-03 14:28:40 -0500

    cleaned up the main

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -12,23 +12,29 @@ public class Controller {
 		readAccountList();
 		Scanner in = new Scanner(System.in);
 		boolean exit = false;
-		System.out.println("Hello");
+		String action = "";
+		System.out.println("Type login if you want to login, logout if you want to exit.");
+		
 		while (!exit) {
-			String action = in.nextLine();
+			action = in.nextLine();
 			action = action.toLowerCase();
 			if (action.equals("logout")) {
 				exit = true;
-			}else if (action.equals("login")) {
+			} else if (action.equals("login")) {
 				currentUser = login();
-			}else if (action.equals("buy"))  {
-				System.out.println("Buy");
-			}else if (action.equals("sell"))  {
+			} else if (action.equals("buy"))  {
+				if (buy()) {
+					System.out.println("You have sucessfully purchased");
+				} else {
+					System.out.println("There was an error when trying to buy");
+				}
+			} else if (action.equals("sell"))  {
 				System.out.println("Sell");
-			}else if (action.equals("addCredit"))  {
+			} else if (action.equals("addCredit"))  {
 				System.out.println("Add credit");
-			}else if (action.equals("refund"))  {
+			} else if (action.equals("refund"))  {
 				System.out.println("Refund");
-			}else if (action.equals("create")) {
+			} else if (action.equals("create")) {
 				System.out.println("Create");
 			} else if (action.equals("delete"))  {
 				System.out.println("Delete");
@@ -38,6 +44,10 @@ public class Controller {
 		in.close();
 	}
 	
+	private static boolean buy() {
+		System.out.println("hello buy man");
+		return true;
+	}
 	private static User login() {
 		Scanner nameIn = new Scanner(System.in);
 		System.out.println("Enter Username: ");
