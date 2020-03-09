c17c0d4ac44901771acf8dab97156fefcd1b149a
kingsleylam <kingsleyl.0120@gmail.com>
2020-03-08 23:53:48 -0400

    stuff

--- src/controller/run.sh
+++ src/controller/run.sh
@@ -9,10 +9,6 @@ javac Controller.java
 for i in {1..72}
 do 
 	echo "running test $i"
-<<<<<<< HEAD
-=======
-	javac Controller.java
->>>>>>> 7aff6007f9fcd0a635c7399d33e2247492e79825
 	java Controller AccountList.txt EventList.txt < "$DIR/Inputs/Test_$i.inp" > "$DIR/Outputs/Output_$i.out"
 done
 
