d49fef53c1691913116076e4d87e45a33ec2b721
Carlson Dultra <38873088+carlsondultra@users.noreply.github.com>
2020-03-08 20:39:04 -0400

    appending content to results file

--- Testing attempt/tester.sh
+++ Testing attempt/tester.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+for i in *.txt;         #for all text files in the directory
+do
+echo "running test $i"
+cat *.txt  >> results.out #appends all text files to results.out
+done
+
+
+
+
+
+
+
 
 
 #take the result file and read in the lines one by one 
-STRING="Hello World"
+#STRING="Hello World"
 
 #make a jar file of the orginial java files (this has to be done before script is ran)
 # write java -cp controller.jar Controller <-- this will run the jar file) 
@@ -11,6 +23,6 @@ STRING="Hello World"
 #feed each line to the running progra
 #compare the results to the results file. if they match, test is passed otherwise test is failed. 
 
-STRING2=$(java -cp test.jar A)
-if [STRING = STRING2]
-	echo "passed"
+#STRING2=$(java -cp test.jar A)
+#if [STRING = STRING2]
+#	echo "passed"
