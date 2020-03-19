982dd9de6f2d51320170819b914b112e9fb9a827
kingsleylam <kingsleyl.0120@gmail.com>
2020-03-19 18:14:08 -0400

    commented the backend

--- src/controller/BackEnd.java
+++ src/controller/BackEnd.java
@@ -2,15 +2,28 @@
 import java.util.*;
 import java.io.*;
 
+/* The component of the assignment that handels all the reading and writing of files
+* @author Kingsley Lam - 500825557
+* @author Carlson Dultra - 500812352 
+* @author Humza Imran - 500522148
+*/
+
 public class BackEnd {
 	private String accountFile;
 	private String eventFile;
-	
+	/* Constructor for the backend files
+	* @param acc A String that contains the name of the account list file
+	* @param event A String that contains the name of the event list file
+	*/
 	public BackEnd(String acc, String event) {
 		accountFile = acc;
 		eventFile = event;
 	}
 
+	/* Reads the pre existing account list file and inputs it into a hashmap
+	* @param filename A String that contians the name of the account list file
+	* @return returns a hashmap that contains all the users mapped out to the username
+	*/
 	public static HashMap<String,User> readAccountList(String filename) {
 		HashMap<String,User> userList = new HashMap<String,User>();
 		try {
@@ -30,7 +43,11 @@ public class BackEnd {
 		}
 		return userList;
 	}
-	
+
+	/* Reads the pre existing event list file and inputs it into a hashmap
+	* @param filename A String that contains the name of the event list file
+	* @return returns a hashmap that contains all the events mapped out to the event name
+	*/
 	public static HashMap<String,Event> readEventList(String filename) {
 		HashMap<String,Event> eventList = new HashMap<String,Event>();
 		try {
@@ -51,7 +68,10 @@ public class BackEnd {
 		return eventList;
 	}
 	
-	
+	/* Writes the updated account list to the accountlist file
+	* @param filename the name of the account list file
+	* @param userList the current users list
+	*/
 	public static void writeAccountList(String filename, HashMap<String, User> userList) {
 		String key;
 		User temp;
@@ -69,6 +89,10 @@ public class BackEnd {
 		}
 	}
 	
+	/* Writes the updated event list to the event list file
+	* @param filename the name of the event list file
+	* @param eventList the current event list
+	*/
 	public static void writeEventList(String filename, HashMap<String, Event> eventList) {
 		String key;
 		Event temp;
@@ -86,6 +110,9 @@ public class BackEnd {
 		}
 	}
 
+	/* Writes out the transaction list of that session
+	* @param transactionList the list the contains all the transaction codes for that session
+	*/
 	public static void writeTransactionList(ArrayList<String> transactionList) {
 		FileWriter fw = null;
         BufferedWriter bw = null;
